0256ee79e39910734530a782e38bc6e2403007eb
Mikey Kriegel <mikkrieg@gmail.com>
2021-01-14 18:33:47 -0800

    Add image, add function to hide form and show letter, add styling to letter

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  
+  $("form").submit(function(e){
+    e.preventDefault();
+    let name = $("input#letter").val();
+    $("span#name").text(name);
+    $(".hide").hide();
+    $("div#hidden").show();
+  });
 });
